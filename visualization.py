9d450c8e18b5c6812b4cc601adcbcea350bb64fd
Fedor <fyodorlllll@gmail.com>
2019-09-11 13:14:31 +0300

    Added forder building for experiments

--- visualization.py
+++ visualization.py
@@ -1,6 +1,8 @@
 from model.engine import MainEngine
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
+from pathlib import Path
 
 
 n_steps = 100
@@ -14,8 +16,14 @@ zeros_cult_cond = []
 rn_cult_cond = []
 energy_hist = []
 first = engine.list_obj[0].culture_condition
+date_now = str(datetime.datetime.now())[:19].replace(':', '_')
+date_now = date_now.replace('-', '_')
+date_now = date_now.replace(' ', '_')
+
+p = Path(f'./images/experiment_' + date_now + '/')
+p.mkdir()
+
 for i in range(n_steps):
-    print(i)
     if i == 0:
         engine.step(energy=energy, update=10)
     else:
@@ -24,11 +32,10 @@ for i in range(n_steps):
     rn_cult_cond.append(np.linalg.norm(engine.list_obj[5].culture_condition.detach().numpy(), 2))
     energy_hist.append(engine.list_obj[0].curr_energy)
 
-    #res = engine.list_obj[0].culture_condition
-    #print(np.linalg.norm(first - res.detach().numpy(), 2))
+
     plt.plot(zeros_cult_cond)
     plt.plot(rn_cult_cond)
-    plt.savefig(f'{i}_graph.png')
-    #plt.show()
+    plt.savefig(f'./images/experiment_{date_now}/{i}_graph.png')
+
 plt.plot(energy_hist)
 plt.show()
